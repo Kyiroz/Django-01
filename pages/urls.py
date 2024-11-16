3dc4865d2f99bd4cd1ba5e241e191911f756b333
Kyiro <josereyesiriarte@gmail.com>
2024-11-16 10:05:39 -0400

    base page done

--- pages/urls.py
+++ pages/urls.py
@@ -3,6 +3,8 @@ from .views import HomePageView, AboutPageView
 
 urlpatterns = [
         #Ruta que se despliega al abrir la aplicacion (la url de la aplicacion)
+        #La ruta vacia en home significa que es la pagina inicial, la ruta llena en about/ significa que es la pagina de about
+        #El parametro name es el pseudonimo que se usara en _base.html
     path('', HomePageView.as_view(), name='home'),
     path("about/", AboutPageView.as_view(), name="about")
 ]
